a17cfd018d52a6f3f5a6a90abbaa2677e85059a0
Ken <kenneth_ljs@live.com>
2021-07-04 00:51:34 +0800

    cleanup

--- src/index-web.ts
+++ src/index-web.ts
@@ -1,3 +1,10 @@
+/*!
+ * domsi <https://github.com/Kenneth-LJS/domsi>
+ *
+ * Copyright (c) 2021-Present, Ken <@Kenneth-LJS> (https://github.com/Kenneth-LJS)
+ * Released under the MIT license.
+ */
+
 import { domsiFind, domsiFindAll } from './domsi/domsi';
 
 export default {
